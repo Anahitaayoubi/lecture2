1c0f32ec35d1b856df8b313fa8bd1b73770838ee
Anahitaayoubi <36800174+Anahitaayoubi@users.noreply.github.com>
2018-03-05 08:35:55 -0800

    Update 6_1_if_statement.py

--- 6_1_if_statement.py
+++ 6_1_if_statement.py
@@ -1,13 +1,14 @@
 ###################################################
 ## these examples shows different type of if-stmt #
 ###################################################
-a=5
-b=6
+a=3
+b=2
+c=4
 if(a == b):
-    print("a == b")
+    print("a+b")
 if(a > b):
-    print("a > b")
+    print("a+b+c")
 elif(a<b):
-    print("a < b")
+    print("c")
 else:
     pass # do nothing
